d97077b728c98edec4f40ac6d3b56b0d41297f80
zeynepucuncuoglu <zeynepucuncuogluu@gmail.com>
2023-12-08 16:18:59 +0300

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Zeynep Üçüncüoğlu on 8.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
